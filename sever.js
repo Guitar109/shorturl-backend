03073b5a7f2b7c8e168b7b3e41a1a0a62ad8121f
Guitar109 <kittipol.le.64@ubu.ac.th>
2024-10-24 01:27:00 +0700

    add.env

--- sever.js
+++ sever.js
@@ -2,14 +2,15 @@ const express = require("express");
 const mongoose = require("mongoose");
 const ShortUrl = require("./model/shortUrls");
 const cors = require('cors'); 
+require('dotenv').config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb://localhost/urlShortener")
+
+mongoose.connect(`${process.env.DATABASE_URL}`)
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...', err));
 
